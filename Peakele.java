adcd3bc247a0c2fb5fc7ce91cdaa9102b7896df9
yoges <yogesht.cs19@bmsce.ac.in>
2022-03-06 22:02:00 +0530

    some interesting stuff check out

--- Peakele.java
+++ Peakele.java
@@ -1,49 +1,20 @@
+import java.util.*;
 public class Peakele {
-    static int peakElement(int[] arr,int n)
-    {
-        int j=-1;
-        for(int i=0; i<n; i++){
-           if(n==1){
-               j = n-1;
-               return j;
-            }else if(n==2){
-                   if(arr[0]>arr[1]){
-                        j = 0;
-                        return j;
-                   }else{
-                       j = 1;
-                       return j;
-                   }
-                        
-            }else{
-                if(i==0){
-                    if(arr[0]>arr[1]){
-                        j = 0;
-                        return j;
-                    }else{
-                        continue;
-                    }
-                }else if(i==n-1){
-                    if(arr[i]>=arr[i-1]){
-                        j = i;
-                        return i;
-                    }
-                }else if((arr[i] >= arr[i-1]) && (arr[i]>= arr[i+1])){
-                    j = i;
-                    return j;
-                }
-                   
-            }
-                    
-        }
-       return j;
-    }
 
     public static void main(String[] args){
-        int arr[] = {17,19,9,5,3,6,17,7,18,16,18,11,3,15,2};
-        int n=15;
-        System.out.println(peakElement(arr,n));
-    }
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        a.forEach(e -> System.out.println(e));
 
-    
+        ArrayList<Integer> b = new ArrayList<>(Collections.nCopies(5,-1));
+        System.out.println(b);
+        b.add(01);
+        b.add(02);
+        b.add(03);
+        b.forEach(e -> System.out.println(e));
+
+        
+    }
 }
