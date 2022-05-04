d71b951689bb33c94bced0122ec70808d7456ed6
Yogesh T <yogesht.cs19@bmsce.ac.in>
2022-05-04 15:54:36 +0530

    complete cast

--- practice.java
+++ practice.java
@@ -16,6 +16,17 @@ class practice {
         l = d.longValue();
         System.out.println(l);
 
+        //cast from double to Long
+        double d1 = 34;
+        Long  l1 = (long)d1; //cast it to primitive long and assign to Long. Auto-boxing will take care after that 
+
+        /**
+         * For cast from Double to byte/short/int/long/double we have .byte/short/int/long/doubleValue() method.
+         * For laborious one see down
+         */
+
+        l = (long)(double)d;
+
     }
 
 }
